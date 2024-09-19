c858cb161e402c2939a7bc0367c8d5c964f6c133
izycki <131556477+luizizycki@users.noreply.github.com>
2024-09-19 18:19:23 -0300

    Adicionado responsividade

--- calculadora/index.js
+++ calculadora/index.js
@@ -2,9 +2,22 @@ const display = document.getElementById("display");
 const historico = document.getElementById("historico");
 historico.value = "";
 display.value = "";
+result = false;
 
 function adicionarAoDisplay(input) {
-  display.value = display.value + input;
+  const operadores = ["+", "-", "/", "*"];
+  const ultimoChar = display.value.slice(0, -1);
+
+  //caso o número for o resultado de outra conta, e o input não for um operador, substituir o resultado pelo input.
+  if (result == true && !operadores.includes(input)) {
+    display.value = input;
+    result = false;
+  } else {
+    display.value = display.value + input;
+    result = false;
+  }
+
+
 }
 
 function limparDisplay() {
@@ -27,7 +40,7 @@ function calcular() {
   try {
     historico.value = historico.value + display.value;
     display.value = eval(display.value);
-
+    result = true;
 
     if (display.value == "Infinity") {
       display.style.color = "red";
@@ -36,10 +49,6 @@ function calcular() {
       sleep(1000).then(() => { display.style.color = "white"; });
     }
 
-    //
-    // TODO: fazer com que o resultado que aparece no display seja apagado caso digite outro número, se for um operador deixa ele lá.
-    //
-
     //tratamento de dizimas periódicas
     let resultado = display.value.toString();
     if (resultado.includes('.')) {
@@ -47,16 +56,17 @@ function calcular() {
       if (format[1].length > 4) {
         display.value = format[0] + '.' + format[1].substring(0, 3);
         historico.value = historico.value + " = " + display.value + "...\n";
+        result = true;
       }
     } else {
       historico.value = historico.value + " = " + display.value + "\n";
+      result = true;
     }
 
   }
   catch (erro) {
     display.style.color = "red";
     display.value = "Error "
-    console.log(erro.message);
     sleep(1000).then(() => { display.value = ""; });
     sleep(1000).then(() => { display.style.color = "white"; });
   }
